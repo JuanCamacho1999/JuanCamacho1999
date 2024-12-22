9a7cbea88e408a5bff82912291eab90badbd4207
JuanCamacho1999 <144348942+JuanCamacho1999@users.noreply.github.com>
2024-12-21 22:07:42 -0500

    prof si no puede recibir este mejor tome el otro
    en este se corrigieron unos errroes, si no lo puede recibir ignorelo y tome el interpretador que si se entrego a tiempo. muy amable, graicas

---
+++ #factorial.py
@@ -0,0 +1,44 @@
+#factorial
+
+def factorial(n):
+    if n < 0:
+        return None
+    if n < 2:
+        return 1
+    return n * factorial(n-1)
+
+print(factorial(3))
+
+print(factorial(4))
+print(factorial(5))
+
+#prueba escritorio
+
+
+def factorial(3):
+    if n < 0:
+        return None
+    if n < 2:
+        return 1
+    return 3 * factorial(3-1)
+
+print(factorial(3))
+
+print(factorial(4))
+print(factorial(5))
+
+def factorial(2):
+    if n < 0:
+        return None
+    if n < 2:
+        return 1
+    return 3 * 2 factorial(2-1)
+
+def factorial(2):
+    if n < 0:
+        return None
+    if n < 2:
+        return 1
+    return 3 * 2 * 1 factorial(2-1)
+
+
---
+++ clase#1.py
@@ -0,0 +1,15 @@
+
+number = [1,2,3,4,5,6,7,8,9,10,12,12,13,12]
+coco= [i for i in number if i%2 == 0]
+print(coco)
+
+
+
+
+print ("Hola mundo")
+
+print("hey")
+
+
+
+
